32bf234c0c97189d56ac0c8022d8f1661f3f6ce8
omprakash <omejjigiri99@gmail.com>
2026-09-05 23:11:33 +0530

    om

--- vars/deployAnsible.groovy
+++ vars/deployAnsible.groovy
@@ -1,7 +1,7 @@
 def call() {
    sshagent(['ansible-controller-ssh']) {
     sh '''
-        ssh -o strictHostkeyChecking=no ubuntu@10-0-1-117 \
+        ssh -o strictHostkeyChecking=no ubuntu@10.0.1.117 \
        "cd ~/ansible_repo && ansible-playbook -i inventory site.yml"
      '''
    }
