1e7debd479dcfe45c4ba368630a9200a0a063582
omprakash <omejjigiri99@gmail.com>
2026-09-05 16:12:21 +0530

    om

--- vars/deployAnsible.groovy
+++ vars/deployAnsible.groovy
@@ -1,3 +1,3 @@
 def call() {
-    sh 'ansible-playbook -i ~/ansible_repo inventory inventory ~/ansible_repo/site.yml'
+    sh 'ansible-playbook -i ~/ansible_repo inventory ~/ansible_repo/site.yml'
 }
